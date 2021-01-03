f2a12c65e2d3a742df3ee33880ee49bd5a02ade8
alestor123 <alestoraldous@gmail.com>
2021-01-03 14:14:11 +0530

    cli

--- App.js
+++ App.js
@@ -2,5 +2,9 @@
 require('dotenv').config()
 
 var express = require('express'),
+axios = require('axios'),
+options  = require('minimist')(process.argv.slice(2)),
 app = express(),
-axios = require('axios');
+api = 'https://api.github.com/graphql',
+port = process.env.PORT || options.port || options.p || 3000,
+token = process.env.TOKEN || options.token || options.t;
